0db0a637e95f9255d2618e728723d6d188450a1e
kudratov001 <nuriddin.k@mvpngn.com>
2022-04-28 14:56:50 +0500

    update on validation

--- client/src/pages/create/CreateUrlPage.tsx
+++ client/src/pages/create/CreateUrlPage.tsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+import { ShortCodeDTO } from "../../models/dto/ShortCodeDTO";
 
 export type CreateUrlPageProps = {};
 export const CreateUrlPage: React.FC<CreateUrlPageProps> = () => {
-    return <>
-      <h2>Create Page</h2>
+  const initialValues: ShortCodeDTO = {
+    url: "",
+    autoGenerate: true,
+    short_code: null
+  }
+
+  const validationSchema = yup.object().shape({
+    url: yup.string().url().required('Url is required'),
+    autoGenerate: yup.boolean().required('Auto generate is required'),
+    short_code: yup.string().when("autoGenerate", { is: false, then: yup.string().min(4, 'Name must be at least 4 characters').matches(/[^a-zA-Z0-9]/, "Name must be character or number").required('Short code is required') })
+  });
+  return <>
+    <main className="create-url ctn-center">
+      <section className="container">
+
+      </section>
+    </main>
   </>
 };
--- server/src/validator/createUrlValidationSchena.ts
+++ server/src/validator/createUrlValidationSchena.ts
@@ -6,5 +6,5 @@ import { ShortCodeDTO } from 'src/models/dto/shortCodeDTO';
 export const patchRuleValidationScheme = yup.object().shape<YupShape<Partial<ShortCodeDTO>>>({
   url: yup.string().url().required('Url is required'),
   autoGenerate: yup.boolean().required('Auto generate is required'),
-  short_code: yup.string().when("autoGenerate", { is: false, then: yup.string().min(4, 'Name must be at least 4 characters').required('Short code is required') })
+  short_code: yup.string().when("autoGenerate", { is: false, then: yup.string().min(4, 'Name must be at least 4 characters').matches(/[^a-zA-Z0-9]/, "Name must be character or number").required('Short code is required') })
 });
