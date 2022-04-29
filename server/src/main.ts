f73679649789cb66676ce5c23b18dfd9ebc3c9d4
kudratov001 <nuriddin.k@mvpngn.com>
2022-04-29 16:12:35 +0500

    enabled cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,8 @@ import { AppModule } from './modules/app.module';
 import { ConfigService } from './services/ConfigService';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get<ConfigService>(ConfigService);
-  app.enableCors();
   await app.listen(configService.config.PORT);
 }
 bootstrap();
