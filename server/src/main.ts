659ca3714a6848aa9989f3ea0f6fd7d908fe1e83
kudratov001 <nuriddin.k@mvpngn.com>
2022-04-29 16:01:08 +0500

    fix on cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,19 +5,7 @@ import { ConfigService } from './services/ConfigService';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
-  app.enableCors({
-    origin(origin, cb) {
-      if(true) {
-        cb(null, true);
-      } else {
-        cb(new Error('Not allowed by CORS!'));
-      }
-    },
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Authorization',
-    methods: 'GET,PUT,PATCH,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(configService.config.PORT);
 }
 bootstrap();
