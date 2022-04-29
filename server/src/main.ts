71d9346a4198298d79604ab54b79d22f250a06a9
kudratov001 <nuriddin.k@mvpngn.com>
2022-04-29 16:49:30 +0500

    enabling cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-const cors = require('cors')
 import { AppModule } from './modules/app.module';
 import { ConfigService } from './services/ConfigService';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
-  app.use(cors());
+  const options = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+  };
+  app.enableCors(options);
   await app.listen(configService.config.PORT);
 }
 bootstrap();
